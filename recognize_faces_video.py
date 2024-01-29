83ea60a5ad5e2103c4a39729e74854637aa2a501
marcosd59 <damian.marcospool@gmail.com>
2024-01-29 08:08:56 -0500

    Script para reconocer rostros en videos

--- recognize_faces_video.py
+++ recognize_faces_video.py
@@ -47,7 +47,6 @@ while True:
         y = top - 15 if top - 15 > 15 else top + 15
         cv2.putText(frame, name, (left, y),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
-
     cv2.imshow('Video', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
