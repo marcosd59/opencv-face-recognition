c5f8127ed9ba0733f2c277b0efe79f64f7601b88
marcosd59 <damian.marcospool@gmail.com>
2024-01-29 08:07:05 -0500

    Update video capture to read from a file

--- recognize_faces_video.py
+++ recognize_faces_video.py
@@ -8,11 +8,19 @@ import cv2
 with open('encodings.pickle', 'rb') as f:
     data = pickle.load(f)
 
-# Iniciar video
-video_capture = cv2.VideoCapture(0)
+# Ruta al archivo de video
+video_path = './unknown_pictures/unknown_2.mp4'
+
+# Iniciar video desde un archivo
+video_capture = cv2.VideoCapture(video_path)
 
 while True:
     ret, frame = video_capture.read()
+
+    # Romper el bucle si no se ha capturado el frame correctamente
+    if not ret:
+        break
+
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     boxes = face_recognition.face_locations(rgb, model='hog')
     encodings = face_recognition.face_encodings(rgb, boxes)
@@ -39,6 +47,7 @@ while True:
         y = top - 15 if top - 15 > 15 else top + 15
         cv2.putText(frame, name, (left, y),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+
     cv2.imshow('Video', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
